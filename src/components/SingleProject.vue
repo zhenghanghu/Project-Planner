1992b7165820a1d78db76596ab9148c454ffe5b7
zhenghang hu <hu4396@terpmail.umd.edu>
2021-06-16 16:33:09 -0400

    implemented delete project

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -3,7 +3,7 @@
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
       <div class="icons">
-          <span class="material-icons">delete</span>
+          <span class="material-icons" @click="deleteProject">delete</span>
           <span class="material-icons">edit</span>
           <span class="material-icons">done</span>
       </div>
@@ -20,8 +20,16 @@ export default {
   data() {
     return {
       showDetails: false,
+      uri: 'http://localhost:3000/projects/' + this.project.id
     }
   },
+  methods:{
+      deleteProject(){
+          fetch(this.uri, {method: 'DELETE'})
+            .then(() => this.$emit('delete', this.project.id))
+            .catch((err) => console.log(err))
+      }
+  }
   
 };
 </script>
